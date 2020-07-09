fix: a part calculation

anl_EnGSM.py
@@ -8,6 +8,7 @@ import os, sys
 sys.path.append(os.path.join(os.path.dirname(__file__), './module'))
 import numpy as np
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 #import my_module
 import mapping
@@ -35,6 +36,7 @@ class Anl_basem:
     surf_elem, elem = RG.data_kind()
     lon, lat = RG.set_coordinate()
     weight_lat = RG.weight_latitude(lat)
+    press_levels = ST.set_pressure_levels()
     elem_num = len(elem)
     
     """Set data. & Making pertubation"""
@@ -61,6 +63,7 @@ class Anl_basem:
       dry_energy_norm[imem-1] = EN.dry_energy_norm(
         pertb_uwnd[imem-1], pertb_vwnd[imem-1],
         pertb_tmp[imem-1], pertb_slp[imem-1],
+        press_levels 
       )
 
     """Calc. rate of Each ensemble member"""
@@ -71,15 +74,14 @@ class Anl_basem:
         area_lon_min =120, area_lon_max =150
     )
 
+    #sum_region_dry_energy_norm = [[] for _ in range(RG.ensemble_size)]
+    #for imem in range(1, RG.ensemble_size):
+    #  sum_region_dry_energy_norm[imem-1] =\
+    #    np.sum(dry_energy_norm[imem-1][lat_min_index:lat_max_index,lon_min_index:lon_max_index])
 
-    sum_region_dry_energy_norm = [[] for _ in range(RG.ensemble_size)]
-    for imem in range(1, RG.ensemble_size):
-      sum_region_dry_energy_norm[imem-1] =\
-        np.sum(dry_energy_norm[imem-1][lat_min_index:lat_max_index,lon_min_index:lon_max_index])
-
-    for _ in range(1, RG.ensemble_size):
-      print(dry_energy_norm[_-1][lat_min_index:lat_max_index, lon_min_index:lon_max_index])
-      print(sum_region_dry_energy_norm[_-1])
+    #for _ in range(1, RG.ensemble_size):
+    #  print(dry_energy_norm[_-1][lat_min_index:lat_max_index, lon_min_index:lon_max_index])
+    #  print(sum_region_dry_energy_norm[_-1])
 
     """Description func. """
     fig, ax = plt.subplots()
@@ -103,10 +105,9 @@ if __name__ == "__main__":
 
   """Class & parm set """
   ST = setup.Setup(dataset)
-  nx, ny, nz, mem, press_levels = ST.set_prm()
-  
+  nx, ny, nz, mem = ST.set_prm()
   RG = readgpv.ReadGPV(nx,ny,nz,mem)
-  EN = readgpv.Energy_norm() 
+  EN = readgpv.Energy_norm(nx,ny) 
   MP = mapping.Mapping('JPN')
 
   if ft == 'anl':

module/readgpv.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from Users.toyo.Terminal.sensitivity_tool.anl_EnGSM import press_levels
 import numpy as np
 from scipy import integrate
 
@@ -49,7 +48,9 @@ class ReadGPV:
     return np.sqrt(np.cos(np.deg2rad(lat)))
 
 class Energy_norm:
-  def __init__(self):
+  def __init__(self, nx, ny):
+    self.nx = nx
+    self.ny = ny
     self.Pr:float=1000.0
     self.Tr:float=270.0
     self.cp:float=1004.0
@@ -78,10 +79,12 @@ class Energy_norm:
     physical_term = (u_prime)**2 + (v_prime)**2
     potential_term = (self.cp/self.Tr)*((tmp_prime)**2)
 
-    first_term = self._vint(physical_term+potential_term, press_levels)
-    sec_term = ((slp_prime/self.Pr)**2)/(2*self.Tr*self.R)   
+    print(physical_term+potential_term)
 
-    dry_energy_norm = first_term + sec_term
+    first_term = self._vint(physical_term+potential_term, press_levels)
+    sec_term = ((self.Tr*self.R)*(slp_prime/self.Pr)**2)/2
+
+    dry_energy_norm = (first_term+sec_term)/(2*self.Tr*self.R) 
     return dry_energy_norm
 
   def humid_energy_norm(self):
@@ -110,11 +113,6 @@ class Energy_norm:
     return area_lat_min_index, area_lat_max_index, \
            area_lon_min_index, area_lon_max_index
 
-  def _multi_prm(self, tmp_prime:np.ndarray, slp_prime:np.ndarray):    
-    multi_prm_tmp_prime = np.sqrt(self.cp/self.Tr)*tmp_prime  
-    multi_prm_slp_prime = (np.sqrt(self.R/self.Tr)/self.Pr)*slp_prime  
-    return multi_prm_tmp_prime, multi_prm_slp_prime
-
   def _vint(self, x_array:np.ndarray, press_array:np.ndarray) -> np.ndarray:
     """気圧面鉛直積分
     Args:
@@ -125,5 +123,10 @@ class Energy_norm:
     Note:
       今回使用した積分方法はシンプソン則
     """
-    y_array = integrate.simps(x_array, press_array)
+    y_array = np.empty_like(x_array[0], dtype=np.float32)
+
+    for iy in range(self.ny):
+      for ix in range(self.nx):
+        y_array[iy,ix] = integrate.simps(x_array[:,iy,ix], press_array[::-1])
+
     return y_array

module/setup.py
@@ -8,8 +8,13 @@ class Setup:
   def set_prm(self):
     if self.dataset is 'WFM':
       nx, ny, nz, mem = 144, 37, 4, 25
-      press_levels = np.array(850.0, 500.0, 300.0)
     elif self.dataset is 'EPSW':
       nx, ny, nz = 144, 73, 4, 27
-      press_levels = np.array(850.0, 500.0, 300.0)
-    return nx, ny, nz, mem, press_levels
+    return nx, ny, nz, mem
+
+  def set_pressure_levels(self):
+    if self.dataset is 'WFM':
+      press_levels = np.array([850.0, 500.0, 300.0])
+    elif self.dataset is 'EPSW':
+      press_levels = np.array([850.0, 500.0, 300.0])
+    return press_levels