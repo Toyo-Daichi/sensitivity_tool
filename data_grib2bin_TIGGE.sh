2db07a0361be92c79403494b1f8ccfe963a21288
Toyooka Daichi <s1920998@s.tsukuba.ac.jp>
2020-08-18 18:31:19 +0900

    fix: tigge code

--- data_grib2bin_TIGGE.sh
+++ data_grib2bin_TIGGE.sh
@@ -23,79 +23,74 @@ case ${center} in
   "NCEP" ) mem=17 ;;
   "ECMWF") mem=50 ;;
   "CMC"  ) mem=20 ;;
+  "UKMO" ) mems=17 ;;
 esac
 
-if [ ${e_dd} -ge ${dd} ]; then
-
-  M=`printf %2.2i ${mm}`
-  while [ ${dd} -le ${e_dd} ]; do
-    D=`printf %2.2i ${dd}`
-    H=`printf %2.2i ${hh}`
-
-    echo '@ READING DAY ' ${yyyy}${M}${D}${H} ' @'
-    
-    o_dir=${outdata_path}/${yyyy}${M}${D}${H}/
-    mkdir -p ${o_dir}
-    i_U_file=${indata_path}/${yyyy}${M}/U_${center}_${yyyy}${M}${D}${H}.grib2
-    i_V_file=${indata_path}/${yyyy}${M}/V_${center}_${yyyy}${M}${D}${H}.grib2
-    i_Z_file=${indata_path}/${yyyy}${M}/Z_${center}_${yyyy}${M}${D}${H}.grib2
-    i_T_file=${indata_path}/${yyyy}${M}/T_${center}_${yyyy}${M}${D}${H}.grib2
-    i_Q_file=${indata_path}/${yyyy}${M}/Q_${center}_${yyyy}${M}${D}${H}.grib2
-    i_PS_file=${indata_path}/${yyyy}${M}/PS_${center}_${yyyy}${M}${D}${H}.grib2
-
-    for ft in ${ft_list[@]}; do
-      i_level=1
-      for level in ${level_list[@]}; do
-        L=`printf %2.2i ${i_level}`
-        if [ ${ft} = "anl" ]; then
-          wgrib2 -v ${i_U_file}  | grep "${level} mb" | grep ":anl:" | wgrib2 ${i_U_file} -i -no_header -append -ieee ${o_dir}/uwnd_${yyyy}${M}${D}${H}_${L}_level.grd
-          wgrib2 -v ${i_V_file}  | grep "${level} mb" | grep ":anl:" | wgrib2 ${i_V_file} -i -no_header -append -ieee ${o_dir}/vwnd_${yyyy}${M}${D}${H}_${L}_level.grd
-          wgrib2 -v ${i_Z_file}  | grep "${level} mb" | grep ":anl:" | wgrib2 ${i_Z_file} -i -no_header -append -ieee ${o_dir}/hgt_${yyyy}${M}${D}${H}_${L}_level.grd
-          wgrib2 -v ${i_T_file}  | grep "${level} mb" | grep ":anl:" | wgrib2 ${i_T_file} -i -no_header -append -ieee ${o_dir}/tmp_${yyyy}${M}${D}${H}_${L}_level.grd
-          wgrib2 -v ${i_Q_file}  | grep "${level} mb" | grep ":anl:" | wgrib2 ${i_Q_file} -i -no_header -append -ieee ${o_dir}/spfh_${yyyy}${M}${D}${H}_${L}_level.grd
-        elif [ ${ft} != "anl" ]; then
-          wgrib2 -v ${i_U_file}  | grep "${level} mb" | grep ":${ft} hour fcst:" | wgrib2 ${i_U_file} -i -no_header -append -ieee ${o_dir}/uwnd_${yyyy}${M}${D}${H}_${L}_level.grd
-          wgrib2 -v ${i_V_file}  | grep "${level} mb" | grep ":${ft} hour fcst:" | wgrib2 ${i_V_file} -i -no_header -append -ieee ${o_dir}/vwnd_${yyyy}${M}${D}${H}_${L}_level.grd
-          wgrib2 -v ${i_Z_file}  | grep "${level} mb" | grep ":${ft} hour fcst:" | wgrib2 ${i_Z_file} -i -no_header -append -ieee ${o_dir}/hgt_${yyyy}${M}${D}${H}_${L}_level.grd
-          wgrib2 -v ${i_T_file}  | grep "${level} mb" | grep ":${ft} hour fcst:" | wgrib2 ${i_T_file} -i -no_header -append -ieee ${o_dir}/tmp_${yyyy}${M}${D}${H}_${L}_level.grd
-          wgrib2 -v ${i_Q_file}  | grep "${level} mb" | grep ":${ft} hour fcst:" | wgrib2 ${i_Q_file} -i -no_header -append -ieee ${o_dir}/spfh_${yyyy}${M}${D}${H}_${L}_level.grd
-        fi
-        
-        #surface pressure
-        if [ ${ft} = "anl" ]; then
-          wgrib2 -v ${i_PS_file} | grep ":surface:" | grep ":anl:" | wgrib2 ${i_PS_file} -i -no_header -append -ieee ${o_dir}/ps_${yyyy}${M}${D}${H}_surf_level.grd
-        elif [ ${ft} != "anl" ]; then
-          wgrib2 -v ${i_PS_file} | grep ":surface:" | grep ":${ft} hour fcst:" | wgrib2 ${i_PS_file} -i -no_header -append -ieee ${o_dir}/ps_${yyyy}${M}${D}${H}_surf_level.grd
-        fi
-        i_level=`expr ${i_level} + 1`
-      done
-
-      #combine
-      if [ ${ft} = "anl" ]; then ft='00' ;fi
-
-      cat ${o_dir}/uwnd_${yyyy}${M}${D}${H}_*_level.grd > ${o_dir}/uwnd_${yyyy}${M}${D}${H}_${ft}hr.grd
-      cat ${o_dir}/vwnd_${yyyy}${M}${D}${H}_*_level.grd > ${o_dir}/vwnd_${yyyy}${M}${D}${H}_${ft}hr.grd
-      cat ${o_dir}/hgt_${yyyy}${M}${D}${H}_*_level.grd  > ${o_dir}/hgt_${yyyy}${M}${D}${H}_${ft}hr.grd
-      cat ${o_dir}/tmp_${yyyy}${M}${D}${H}_*_level.grd  > ${o_dir}/tmp_${yyyy}${M}${D}${H}_${ft}hr.grd
-      cat ${o_dir}/spfh_${yyyy}${M}${D}${H}_*_level.grd > ${o_dir}/spfh_${yyyy}${M}${D}${H}_${ft}hr.grd
-      cat ${o_dir}/ps_${yyyy}${M}${D}${H}_*_level.grd   > ${o_dir}/ps_${yyyy}${M}${D}${H}_${ft}hr.grd
-    
-      cat ${o_dir}/uwnd_${yyyy}${M}${D}${H}_${ft}hr.grd \
-          ${o_dir}/vwnd_${yyyy}${M}${D}${H}_${ft}hr.grd \
-          ${o_dir}/hgt_${yyyy}${M}${D}${H}_${ft}hr.grd  \
-          ${o_dir}/tmp_${yyyy}${M}${D}${H}_${ft}hr.grd  \
-          ${o_dir}/spfh_${yyyy}${M}${D}${H}_${ft}hr.grd \
-          ${o_dir}/ps_${yyyy}${M}${D}${H}_${ft}hr.grd   \
-        > ${o_dir}/${yyyy}${M}${D}${H}_${ft}hr_${mem}mem.grd
-
-     rm ${o_dir}/*_${yyyy}${M}${D}${H}_*_level.grd ${o_dir}/*_${yyyy}${M}${D}${H}_${ft}hr.grd
-     sleep 5.0s
-
-    done
-    dd=`expr ${dd} + 1`
+M=`printf %2.2i ${mm}`
+while [ ${dd} -le ${e_dd} ]; do
+  D=`printf %2.2i ${dd}`
+  H=`printf %2.2i ${hh}`
+  echo '@ READING DAY ' ${yyyy}${M}${D}${H} ' @'
   
-  done
+  o_dir=${outdata_path}/${yyyy}${M}${D}${H}/
+  mkdir -p ${o_dir}
+  i_U_file=${indata_path}/${yyyy}${M}/U_${center}_${yyyy}${M}${D}${H}.grib2
+  i_V_file=${indata_path}/${yyyy}${M}/V_${center}_${yyyy}${M}${D}${H}.grib2
+  i_Z_file=${indata_path}/${yyyy}${M}/Z_${center}_${yyyy}${M}${D}${H}.grib2
+  i_T_file=${indata_path}/${yyyy}${M}/T_${center}_${yyyy}${M}${D}${H}.grib2
+  i_Q_file=${indata_path}/${yyyy}${M}/Q_${center}_${yyyy}${M}${D}${H}.grib2
+  i_PS_file=${indata_path}/${yyyy}${M}/PS_${center}_${yyyy}${M}${D}${H}.grib2
 
-fi
+  for ft in ${ft_list[@]}; do
+    i_level=1
+    
+    for level in ${level_list[@]}; do
+      L=`printf %2.2i ${i_level}`
+      if [ ${ft} = "anl" ]; then
+        wgrib2 -v ${i_U_file}  | grep "${level} mb" | grep ":anl:" | wgrib2 ${i_U_file} -i -no_header -append -ieee ${o_dir}/uwnd_${yyyy}${M}${D}${H}_${L}_level.grd
+        wgrib2 -v ${i_V_file}  | grep "${level} mb" | grep ":anl:" | wgrib2 ${i_V_file} -i -no_header -append -ieee ${o_dir}/vwnd_${yyyy}${M}${D}${H}_${L}_level.grd
+        wgrib2 -v ${i_Z_file}  | grep "${level} mb" | grep ":anl:" | wgrib2 ${i_Z_file} -i -no_header -append -ieee ${o_dir}/hgt_${yyyy}${M}${D}${H}_${L}_level.grd
+        wgrib2 -v ${i_T_file}  | grep "${level} mb" | grep ":anl:" | wgrib2 ${i_T_file} -i -no_header -append -ieee ${o_dir}/tmp_${yyyy}${M}${D}${H}_${L}_level.grd
+        wgrib2 -v ${i_Q_file}  | grep "${level} mb" | grep ":anl:" | wgrib2 ${i_Q_file} -i -no_header -append -ieee ${o_dir}/spfh_${yyyy}${M}${D}${H}_${L}_level.grd
+      elif [ ${ft} != "anl" ]; then
+        wgrib2 -v ${i_U_file}  | grep "${level} mb" | grep ":${ft} hour fcst:" | wgrib2 ${i_U_file} -i -no_header -append -ieee ${o_dir}/uwnd_${yyyy}${M}${D}${H}_${L}_level.grd
+        wgrib2 -v ${i_V_file}  | grep "${level} mb" | grep ":${ft} hour fcst:" | wgrib2 ${i_V_file} -i -no_header -append -ieee ${o_dir}/vwnd_${yyyy}${M}${D}${H}_${L}_level.grd
+        wgrib2 -v ${i_Z_file}  | grep "${level} mb" | grep ":${ft} hour fcst:" | wgrib2 ${i_Z_file} -i -no_header -append -ieee ${o_dir}/hgt_${yyyy}${M}${D}${H}_${L}_level.grd
+        wgrib2 -v ${i_T_file}  | grep "${level} mb" | grep ":${ft} hour fcst:" | wgrib2 ${i_T_file} -i -no_header -append -ieee ${o_dir}/tmp_${yyyy}${M}${D}${H}_${L}_level.grd
+        wgrib2 -v ${i_Q_file}  | grep "${level} mb" | grep ":${ft} hour fcst:" | wgrib2 ${i_Q_file} -i -no_header -append -ieee ${o_dir}/spfh_${yyyy}${M}${D}${H}_${L}_level.grd
+      fi
+      
+      #surface pressure
+      if [ ${ft} = "anl" ]; then
+        wgrib2 -v ${i_PS_file} | grep ":surface:" | grep ":anl:" | wgrib2 ${i_PS_file} -i -no_header -append -ieee ${o_dir}/ps_${yyyy}${M}${D}${H}_surf_level.grd
+      elif [ ${ft} != "anl" ]; then
+        wgrib2 -v ${i_PS_file} | grep ":surface:" | grep ":${ft} hour fcst:" | wgrib2 ${i_PS_file} -i -no_header -append -ieee ${o_dir}/ps_${yyyy}${M}${D}${H}_surf_level.grd
+      fi
+      i_level=`expr ${i_level} + 1`
+    done
+    #combine
+    if [ ${ft} = "anl" ]; then ft='00' ;fi
+    cat ${o_dir}/uwnd_${yyyy}${M}${D}${H}_*_level.grd > ${o_dir}/uwnd_${yyyy}${M}${D}${H}_${ft}hr.grd
+    cat ${o_dir}/vwnd_${yyyy}${M}${D}${H}_*_level.grd > ${o_dir}/vwnd_${yyyy}${M}${D}${H}_${ft}hr.grd
+    cat ${o_dir}/hgt_${yyyy}${M}${D}${H}_*_level.grd  > ${o_dir}/hgt_${yyyy}${M}${D}${H}_${ft}hr.grd
+    cat ${o_dir}/tmp_${yyyy}${M}${D}${H}_*_level.grd  > ${o_dir}/tmp_${yyyy}${M}${D}${H}_${ft}hr.grd
+    cat ${o_dir}/spfh_${yyyy}${M}${D}${H}_*_level.grd > ${o_dir}/spfh_${yyyy}${M}${D}${H}_${ft}hr.grd
+    cat ${o_dir}/ps_${yyyy}${M}${D}${H}_*_level.grd   > ${o_dir}/ps_${yyyy}${M}${D}${H}_${ft}hr.grd
+  
+    cat ${o_dir}/uwnd_${yyyy}${M}${D}${H}_${ft}hr.grd \
+        ${o_dir}/vwnd_${yyyy}${M}${D}${H}_${ft}hr.grd \
+        ${o_dir}/hgt_${yyyy}${M}${D}${H}_${ft}hr.grd  \
+        ${o_dir}/tmp_${yyyy}${M}${D}${H}_${ft}hr.grd  \
+        ${o_dir}/spfh_${yyyy}${M}${D}${H}_${ft}hr.grd \
+        ${o_dir}/ps_${yyyy}${M}${D}${H}_${ft}hr.grd   \
+      > ${o_dir}/${yyyy}${M}${D}${H}_${ft}hr_${mem}mem.grd
+
+   rm ${o_dir}/*_${yyyy}${M}${D}${H}_*_level.grd ${o_dir}/*_${yyyy}${M}${D}${H}_${ft}hr.grd
+   
+   sleep 5.0s
+  done
+  dd=`expr ${dd} + 1`
+
+done
 
 exit
