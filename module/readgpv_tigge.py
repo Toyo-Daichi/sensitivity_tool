fix: normalization

module/readgpv_tigge.py
@@ -345,7 +345,7 @@ class Energy_NORM:
       normalize_eigen_vector(np.ndarray): 正規化された固有ベクトル
     """
     normalize_eigen_vector = statics_tool.normalize(eigen_vector,axis=None) 
-    print('..... CHECK EIGENVECTOR SUM :: ', normalize_eigen_vector*normalize_eigen_vector.sum()
+    print('..... CHECK EIGENVECTOR SUM :: {:.2f}'.format(normalize_eigen_vector*normalize_eigen_vector.sum()))
 
     return normalize_eigen_vector
     